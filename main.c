febe44abccf4fb146fd2bffb28b9bb1f7564fdf6
lannoene <lannoene@gmail.com>
2024-03-07 21:05:45 -0800

    Peer to peer transfer now possible

--- main.c
+++ main.c
@@ -13,9 +13,12 @@
 
 #include <poll.h>
 
+#include "conio.h"
+
 #define MAX_CLIENTS 10
 #define DEFAULT_PORT 4659
 #define STATIC_BUF_SIZE 4096
+//#define STATIC_BUF_SIZE 128
 
 #define NETHEADER_SIZE ((int)(sizeof(uint32_t) + sizeof(enum packet_type)))
 #define CLAMP(min, d, max) \
@@ -49,6 +52,7 @@ struct Server {
 	uint16_t port;
 	int sock;
 	struct pollfd poll;
+	bool isHosting;
 } host;
 
 struct NetMsg {
@@ -78,23 +82,22 @@ bool ConnectToPeer(const char *ip, uint16_t port);
 void SendNetMsg(enum packet_type type, void *data, uint32_t size);
 bool ReceiveFile(char *fileBuffer, int fileSize);
 void SendFile(const char *filename);
-
-bool sendFile = false;
+void DrawDownloadProgress(int percent);
+void DrawTitleScreen();
 
 int main(int argc, char* argv[]) {
 	if (!HostInit(DEFAULT_PORT))
 		puts("Could not init socket! Sorry! (this probably means that the ports have already been taken)");
 	
-	puts("Enter mode: (r recv, s send)");
+	puts("Enter mode: (j join, h host)");
 	while (true) {
-		char opt = getchar();
-		if (opt == 's') {
-			sendFile = true;
-			puts("Send mode");
-			char sendFilename[50];
-			puts("Enter filename to send:");
-			scanf("%s", sendFilename);
-			puts("Enter ip & port to send to (fmt ip:port or type localhost):");
+		char opt = c_getch();
+		if (opt == 'j') {
+			host.isHosting = false;
+			
+			//sendFile = true;
+			puts("You are a client");
+			puts("Enter ip & port to connect to (fmt ip:port or type localhost):");
 			char sendToIp[50];
 			uint16_t sendToPort;
 			scanf("%s", sendToIp); // scanf sucks omg i have to do everything myself
@@ -115,64 +118,72 @@ int main(int argc, char* argv[]) {
 				sendToPort = DEFAULT_PORT;
 			}
 			printf("ip: %s, port: %hu\n", sendToIp, sendToPort);
-			if (ConnectToPeer(sendToIp, sendToPort)) {
-				SendFile(sendFilename);
+			
+			if (!ConnectToPeer(sendToIp, sendToPort)) {
+				puts("Could not connect to host. Please renter information.");
+				puts("Enter mode: (j join, h host)"); // jank
+			} else {
+				puts("Successfully connected to host.");
+				break;
 			}
-			break;
-		} else if (opt == 'r') {
-			puts("Receive mode");
+		} else if (opt == 'h') {
+			puts("You are the host");
+			host.isHosting = true;
 			break;
 		}
 	}
-	//puts("key presesd");
+	puts("To send a file, press s");
 	
 	while (!b_exit) {
-		bool hasFoundConn = false;
-		if (!sendFile) { // if we aren't sending a file listen
-			while (!hasFoundConn) {
-				hasFoundConn = ListenForConns();
+		//puts("new");
+		if (c_kbhit() && c_getch() == 's') {
+			char sendFilename[50];
+			puts("Enter filename to send:");
+			scanf("%s", sendFilename);
+			SendFile(sendFilename);
+		}
+		if (host.isHosting) {
+			//puts("listening for conns");
+			if (ListenForConns()) {
+				printf("Peer connected!\n");
 			}
-			printf("Peer connected!\n");
-			bool listenForData = true;
-			while (listenForData) {
-				char buffer[STATIC_BUF_SIZE];
-				if (ListenForNetMsgs(buffer)) {
-					/*FILE* file = fopen("file.bin", "wb");
-					fwrite(buffer, 44, 1, file);
-					fclose(file);*/
-					enum packet_type type = ((struct NetMsg*)buffer)->type;
-					char *packData = ((struct NetMsg*)buffer)->data;
-					uint32_t packSize = ((struct NetMsg*)buffer)->size;
-					switch (type) {
-						default:
-							printf("Unkown data type: %d", type);
-							break;
-						case FILE_HEADER:
-							// get relevant data out of packet
-							int fileSize = ((struct FileInfo*)(packData))->size;
-							char filename[30];
-							strncpy(filename, ((struct FileInfo*)(packData))->filename, 30);
-							printf("Downloading file:\nSize: %d\nName: %s\n", fileSize, filename);
-							// tell peer that we got their request and we're okay with it
-							SendNetMsg(FILE_REQ_ACK, "", 0);
-							char *fileBuffer = malloc(fileSize);
-							// receive file
-							if (ReceiveFile(fileBuffer, fileSize)) { // if file receive did not time out, save it
-								puts("Finished receiving file :)");
-								FILE *file = fopen(filename, "wb");
-								fwrite(fileBuffer, fileSize, 1, file);
-								fclose(file);
-							} else {
-								puts("An error has occured! Could not receive file :(");
-							}
-							free(fileBuffer);
-							break;
+		}
+		bool listenForData = true;
+		char buffer[STATIC_BUF_SIZE];
+		int bytesRecv = 0;
+		while (bytesRecv = ListenForNetMsgs(buffer)) {
+			if (bytesRecv == -1) { // on error
+				b_exit = true;
+				break;
+			}
+			enum packet_type type = ((struct NetMsg*)buffer)->type;
+			char *packData = ((struct NetMsg*)buffer)->data;
+			uint32_t packSize = ((struct NetMsg*)buffer)->size;
+			switch (type) {
+				default:
+					printf("Unkown data type: %d", type);
+					break;
+				case FILE_HEADER:
+					// get relevant data out of packet
+					int fileSize = ((struct FileInfo*)(packData))->size;
+					char filename[30];
+					strncpy(filename, ((struct FileInfo*)(packData))->filename, 30);
+					printf("Downloading file:\nSize: %d\nName: %s\n", fileSize, filename);
+					// tell peer that we got their request and we're okay with it
+					SendNetMsg(FILE_REQ_ACK, "", 0);
+					char *fileBuffer = malloc(fileSize);
+					// receive file
+					if (ReceiveFile(fileBuffer, fileSize)) { // if file receive did not time out, save it
+						puts("Finished receiving file :)");
+						FILE *file = fopen(filename, "wb");
+						fwrite(fileBuffer, fileSize, 1, file);
+						fclose(file);
+					} else {
+						puts("An error has occured! Could not receive file :(");
 					}
-				}
+					free(fileBuffer);
+					break;
 			}
-			puts("Finished?");
-		} else { // if we are, actively connect
-			
 		}
 	}
 	
@@ -243,7 +254,7 @@ int ListenForConns(void) {
 
 	if (pollRet < 0) {
 		//lastErr = BIND_ERR;
-		return 1;
+		return 0;
 	} else if (pollRet == 0) {
 		// no data to be read. do nothing
 	} else {
@@ -252,11 +263,11 @@ int ListenForConns(void) {
 
 		if (host.poll.revents != POLLIN) { // unexpected result
 			//lastErr = POLL_ERR_UNEXP_LISTEN;
-			return 1;
+			return 0;
 		}
 		int acceptRet = AcceptPeer();
 		if (acceptRet == -1)
-			return 1;
+			return 0;
 		//puts("Client connected!");
 		AddPeer(acceptRet);
 		return true;
@@ -326,10 +337,10 @@ bool ConnectToPeer(const char *ip, uint16_t port) {
 	
 	// connect socket to server
 	if (connect(host.peer.sock, (struct sockaddr*)&peerInfo, sizeof(peerInfo)) == 0) {
-		puts("Successfully connected to peer.");
+		//puts("Successfully connected to peer.");
 		host.peer.status = IDLING;
 	} else {
-		puts("Could not connect to peer.");
+		//puts("Could not connect to peer.");
 		close(host.peer.sock);
 		return false;
 	}
@@ -339,6 +350,10 @@ bool ConnectToPeer(const char *ip, uint16_t port) {
 
 void SendFile(const char *filename) {
 	FILE *file = fopen(filename, "rb");
+	if (file == NULL) {
+		printf("Could not open file '%s'. Consider checking the filename and rentering the information.\n", filename);
+		return;
+	}
 	fseek(file, 0L, SEEK_END);
 	int fileSize = ftell(file);
 	rewind(file);
@@ -367,6 +382,7 @@ void SendFile(const char *filename) {
 }
 
 bool ReceiveFile(char *fileBuffer, int fileSize) {
+	printf("Progress:\n\n\n");
 	int receivedBytes = 0;
 	int receivedFileData = -NETHEADER_SIZE; // skip netheader
 	
@@ -386,13 +402,38 @@ bool ReceiveFile(char *fileBuffer, int fileSize) {
 				receivedFileData += bytesRead;
 			}
 			float progress = CLAMP(0, ((float)receivedFileData/fileSize)*100, fileSize);
-			if ((int)progress % (100/4) == 0 && (int)progress != lastGivenProgress) {
-				printf("%.2f%% done\n", progress);
+			if ((int)progress % (1) == 0 && (int)progress != lastGivenProgress) {
+				//printf("%.2f%% done\n", progress);
+				DrawDownloadProgress(progress);
 				lastGivenProgress = (int)progress;
 			}
 		}
 		
 	}
 	
+	printf("\n");
+	
 	return true;
+}
+
+void DrawDownloadProgress(int percent) {
+	struct text_info termInfo;
+	c_gettextinfo(&termInfo);
+	c_gotoxy(0, termInfo.cury - 2);
+	for (int i = 0; i < termInfo.screenwidth - 1; i++) { // print top bar
+		printf("-");
+	}
+	//c_gotoxy(0, termInfo.screenheight - 1);
+	printf("\n");
+	for (int i = 0; i < termInfo.screenwidth - 1; i++) { // print bottom bar
+		if (((float)i/termInfo.screenwidth)*100 <= percent) // if the given percent of with is less than the percent progress
+			printf("#"); // draw a bar
+		else
+			printf(" "); // otherwise draw a space
+	}
+	//c_gotoxy(0, termInfo.screenheight);
+	printf("\n");
+	for (int i = 0; i < termInfo.screenwidth - 1; i++) { // print progress bar
+		printf("-");
+	}
 }
